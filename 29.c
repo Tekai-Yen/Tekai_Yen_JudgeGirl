41cdbbc20b96d016139cb62e6e0792d312647b48
Tekai Yen <tekai324.yen@gmail.com>
2023-11-14 00:00:13 +0800

    Update 29.c
    try out on 2023/11/14

--- 29.c
+++ 29.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
 
+int tekai(int r, int c){
+    //printf("%d %d\n", r, c);
+    if(r == 0 || c == 0) return 1;
+    else return tekai(r-1, c) + tekai(r, c-1);
+}
+
 int main()
 {
-    
-
+    int r, c;
+    scanf("%d%d", &r, &c);
+    printf("%d", tekai(r-1, c-1));
     return 0;
 }
